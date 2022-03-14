c39e8afd0fbf5b938b7db240a0d645a49ef05d5c
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-14 15:37:40 +0100

    Ho creato la funzione per il prompt di una parola

--- palindromi/js/script.js
+++ palindromi/js/script.js
@@ -1,1 +1,18 @@
 console.log('JS OK!');
+
+//Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
+// creo una funzione per far scrivere una parola all'utente
+function userWriteWord(message) {
+    
+    const createWord = prompt(message);
+
+    return createWord
+}
+
+
+const userWord = userWriteWord('Inserisci una parola');
+
+console.log(userWord);
