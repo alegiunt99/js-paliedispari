5e74dc522f5d66c6e3439c026e3220542405977e
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-14 15:57:25 +0100

    Ho creato una funzione per verificare che la parola sia palindroma

--- palindromi/js/script.js
+++ palindromi/js/script.js
@@ -16,3 +16,29 @@ function userWriteWord(message) {
 const userWord = userWriteWord('Inserisci una parola');
 
 console.log(userWord);
+
+const palindromeOrNot = checkPalindrome(userWord);
+
+console.log(palindromeOrNot);
+
+// ora devo creare una funzione che mi permetta di capire se la parola è palindroma
+
+function checkPalindrome(string) {
+
+    // calcolo la lunghezza di una stringa
+
+    const stringLength = string.length;
+
+    //creo un loop che divide a metà la stringa
+    for (let i = 0; i < stringLength / 2; i++){
+
+        //controllo se le due metà della stringa sono uguali
+        if (string[i] !== string[stringLength - 1 - i]) { // se la prima metà e l'altra sono diversi
+
+            return 'NON è un palindromo';
+        }
+    }
+    // una volta usciti dal ciclo, ho la certezza di avere un polindromo
+    
+    return 'è un palindromo';
+}
